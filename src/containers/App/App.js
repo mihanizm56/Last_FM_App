96d3d264fd5cf26925c3d5f5b637586882bc2d3d
mihanizm56 <m727507.56@mail.ru>
2018-11-26 16:56:06 +0400

    making blocks

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from "react";
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
-import { ArtistBox } from "../../modules/ArtistBox/artistBox";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div className="App">
-          <ArtistBox />
-        </div>
+        <div className="App" />
       </BrowserRouter>
     );
   }
