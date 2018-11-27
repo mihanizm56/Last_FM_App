9324311da7a16e2af53cb6a4f3391974b7602faf
mihanizm56 <m727507.56@mail.ru>
2018-11-27 13:24:51 +0400

    fixed

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,7 +2,30 @@ import React, { Component } from "react";
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import { LogoBlock } from "../../modules/LogoBlock/logoBlock";
-import { ArtistsPage } from "../../containers/ArtristsPage/artistsPage"
+import { ArtistsPage } from "../../containers/ArtristsPage/artistsPage";
+
+const arrayListArtists = [
+  { data: "test1" }, ///для примера завёл такой массив, по идее мы его получаем после запроса на сервер
+  { data: "test2" },
+  { data: "test3" },
+  { data: "test4" },
+  { data: "test5" },
+  { data: "test6" },
+  { data: "test7" },
+  { data: "test8" },
+  { data: "test9" },
+  { data: "test10" },
+  { data: "test11" },
+  { data: "test12" },
+  { data: "test13" },
+  { data: "test14" },
+  { data: "test15" },
+  { data: "test16" },
+  { data: "test17" },
+  { data: "test18" },
+  { data: "test19" },
+  { data: "test20" }
+];
 
 class App extends Component {
   render() {
@@ -10,7 +33,7 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
           <LogoBlock />
-          <ArtistsPage />
+          <ArtistsPage arrayListArtists={arrayListArtists} />
         </div>
       </BrowserRouter>
     );
--- src/containers/ArtristsPage/artistsPage.js
+++ src/containers/ArtristsPage/artistsPage.js
@@ -8,38 +8,17 @@ import "./artistsPage.css";
 
 export class ArtistsPage extends Component {
   state = {
-    arrayListArtists: [
-      { data: "test1" }, ///для примера завёл такой массив, по идее мы его получаем после запроса на сервер
-      { data: "test2" },
-      { data: "test3" },
-      { data: "test4" },
-      { data: "test5" },
-      { data: "test6" },
-      { data: "test7" },
-      { data: "test8" },
-      { data: "test9" },
-      { data: "test10" },
-      { data: "test11" },
-      { data: "test12" },
-      { data: "test13" },
-      { data: "test14" },
-      { data: "test15" },
-      { data: "test16" },
-      { data: "test17" },
-      { data: "test18" },
-      { data: "test19" },
-      { data: "test20" }
-    ],
     from: 0,
     to: 9
   };
 
   getPropsForSlice = array => {
-    this.setState({ ...this.state, from: array[1], to: array[2] });
+    this.setState({ from: array[1], to: array[2] });
   };
 
   render() {
-    const { arrayListArtists, from, to } = this.state;
+    const { arrayListArtists } = this.props;
+    const { from, to } = this.state;
     return (
       <div className="artistsPage">
         <div className="artistsPage-title">
