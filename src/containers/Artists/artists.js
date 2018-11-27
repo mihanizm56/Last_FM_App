5979f5f3d933bb0f06040a12788496dfc0018fcb
mihanizm56 <m727507.56@mail.ru>
2018-11-28 01:28:42 +0400

    making artists page

--- src/containers/Artists/artists.js
+++ src/containers/Artists/artists.js
@@ -8,43 +8,73 @@ import LastFM from "last-fm";
 import "./artists.css";
 
 export default class Artists extends Component {
-  state = {
-    currentPage: 1,
-    api_key: "87d932f5f0f2d91d94deb48a52f3c83a",
-    limitPerPage: 9,
-    arrayListArtists: []
-  };
+  constructor() {
+    super();
+    this.state = {
+      currentPage: 1,
+      arrayListArtists: [],
+      paginationIsVisible: true
+    };
+
+    this.api_key = "f65384e5d8c2e5dfd1458348d592a516";
+    this.limitPerPage = 9;
+
+    this.lastfm = new LastFM(this.api_key, {
+      userAgent: "MyApp/1.0.0 (http://example.com)"
+    });
+  }
 
   componentDidMount() {
     this.getArtists();
   }
 
   getArtists = (page = 1) => {
-    console.log(`page = ${typeof page}`);
-    const lastfm = new LastFM("87d932f5f0f2d91d94deb48a52f3c83a", {
-      userAgent: "MyApp/1.0.0 (http://example.com)"
-    });
+    console.log(`page = ${page}`);
+    const api_key = this.api_key;
+    const limitPerPage = this.limitPerPage;
 
-    const { limitPerPage, api_key } = this.state;
-
-    lastfm.chartTopArtists(
-      { page, limit: limitPerPage, api_key },
+    this.lastfm.chartTopArtists(
+      { page: page, limit: limitPerPage, api_key: api_key },
       (err, data) => {
         if (data) {
           if (data.result.length > 9) {
+            console.log(data.result);
             return this.setState({
               ...this.state,
               arrayListArtists: data.result.slice(-9),
-              currentPage: page
+              currentPage: page,
+              paginationIsVisible: true
             });
           } else
-          return this.setState({
+            return this.setState({
               ...this.state,
               arrayListArtists: data.result,
-              currentPage: page
+              currentPage: page,
+              paginationIsVisible: true
             });
         }
         console.error(err);
+      }
+    );
+  };
+
+  getFoundTracks = string => {
+      if(string==='') return this.getArtists()
+    console.log("check getFoundTracks");
+    const key = this.api_key;
+    const page = this.state.currentPage;
+
+    this.lastfm.search(
+      { artist: string, api_key: key, page: page, q: string },
+      (err, data) => {
+        if (data) {
+          console.log(data.result);
+          this.setState({
+            ...this.state,
+            arrayListArtists: data.result.artists,
+            paginationIsVisible: false
+          });
+        } else console.log(err);
       }
     );
   };
@@ -55,27 +85,36 @@ export default class Artists extends Component {
     this.setState({ ...this.state, currentPage: page });
   };
 
+
+
   render() {
-    const { arrayListArtists } = this.state;
+    const { arrayListArtists, paginationIsVisible } = this.state;
     return (
-      <>
+      <div className="artistsPage-wrapper">
         <div className="artistsPage">
           <div className="artistsPage-title">
             <TitleH2 name="Артисты" />
           </div>
           <div className="artistsPage-filters">
-            <SearchField placeholder="Поиск артиста" />
-            <FilterGenres />
+            <SearchField
+              placeholder="Поиск артиста"
+              callback={this.getFoundTracks}
+            />
+            {/* <FilterGenres /> */}
           </div>
         </div>
         <div className="artistsPage-artistsList">
           <ArtistsList list={arrayListArtists} />
         </div>
-        <PaginationBlock
-          artistsList={arrayListArtists}
-          callbackForPagging={this.getArtists}
-        />
-      </>
+        {paginationIsVisible ? (
+          <div className="PaginationBlock-wrapper">
+            <PaginationBlock
+              artistsList={arrayListArtists}
+              callbackForPagging={this.getArtists}
+            />
+          </div>
+        ) : null}
+      </div>
     );
   }
 }
