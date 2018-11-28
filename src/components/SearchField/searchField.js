91d8604db82c3120573cd09008d40dfe713781f5
mihanizm56 <m727507.56@mail.ru>
2018-11-28 23:52:47 +0300

    fixing Album

--- src/components/SearchField/searchField.js
+++ src/components/SearchField/searchField.js
@@ -2,14 +2,16 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./searchField.css";
 
-export const SearchField = ({ placeholder, callback }) => {
-    return <input className="input-field" placeholder={placeholder} onChange={ input => callback(input.target.value)}/>;
+export const SearchField = ({ placeholder, callback, style }) => {
+  return <input className="input-field" style={style} placeholder={placeholder} onChange={input => callback(input.target.value)} />;
 };
 
 SearchField.defaultProps = {
-  placeholder: "placeholder is empty"
+  placeholder: "placeholder is empty",
+  style:{}
 };
 
 SearchField.propTypes = {
-  placeholder: PropTypes.string.isRequired
+  placeholder: PropTypes.string.isRequired,
+  style: PropTypes.object.isRequired
 };
