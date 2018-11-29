a2c28a95e2e41c1b5358a4e5814b4b4d8efd230e
shestopalova <libashestopalova@mail.ru>
2018-11-29 14:08:42 +0300

    got artist name to get information about artist

--- src/containers/Artist/artist.js
+++ src/containers/Artist/artist.js
@@ -4,6 +4,7 @@ import "./artist.css";
 import LastFM from "last-fm";
 import {AlbumList} from "../../modules/AlbumList/albumList";
 import {TrackList} from "../../modules/TracksList/trackList";
+import queryString from "query-string"
 
 const API_KEY = `659beef5a99f79b12854cc654f94b0d5`
 
@@ -15,25 +16,27 @@ class Artist extends Component {
     }
 
     componentDidMount(){
+        let artistName = ""
+        if(this.props.location){
+            const parsed = queryString.parse(this.props.location.search)
+            artistName = parsed.artist
+        }
         this.lastfm = new LastFM(API_KEY)
-        this.getArtistInfo()
+        this.getArtistInfo(artistName)
     }
 
-    getArtistInfo(name = "Tree days grace"){
+    getArtistInfo(name){
         this.lastfm.artistInfo({name, API_KEY}, (err, data) => {
             if (err) console.error(err)
             else {
-                console.log(data)
                 const newArtistInfo = {name: data.name, image: data.images[1]}
                 this.lastfm.artistTopAlbums({name, API_KEY}, (err, data) => {
                     if (err) console.error(err)
                     else {
-                        console.log(data.result)
                         const topAlbum = data.result
                         this.lastfm.artistTopTracks({name, API_KEY}, (err, data) => {
                             if (err) console.error(err)
                             else {
-                                console.log(data.result)
                                 this.setState({
                                     topAlbums: topAlbum,
                                     artistInfo: newArtistInfo,
