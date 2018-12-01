c6fccee44d31e1474693b37bfe63c2a71040245b
mihanizm56 <m727507.56@mail.ru>
2018-12-02 02:24:14 +0400

    fixed

--- src/components/BounceLoading/index.js
+++ src/components/BounceLoading/index.js
@@ -1,6 +1,6 @@
-import React from "react"
-import "./BounceLoading.css"
-import rotateIcon from "./img/bounce.svg"
+import React from 'react'
+import './BounceLoading.css'
+import rotateIcon from './img/bounce.svg'
 
 export const BounceLoading = () => {
   return <img alt="loading..." src={rotateIcon} className="loading-round" />
