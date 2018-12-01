452491dcc606dc4bf245305e545f8aa4af77cde6
Nikita Efimenko <mszx2000@gmail.com>
2018-12-02 03:35:13 +0400

    test-config-init

---
+++ src/components/SearchField/index.test.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import  { SearchField }  from '.'
+
+describe('SearchField', () => {
+	it('Нормальный жизненный цикл', () => {
+		const component = shallow(
+			<SearchField placeholder='lala' 
+						style={{background: '#444'}} 
+						callback={ () => 'callback'}/>)
+		expect(component).toMatchSnapshot()
+		component.mount()
+		component.umount()
+	})
+})
