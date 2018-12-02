2075feb4dc97e43b35559d72f8d5bc594881270e
jilkinalexandr <jilkinalexandr@gmail.com>
2018-12-02 17:28:59 +0300

    добавлены тесты для компонентов SmallImage и TitleH2, добавлена dev-зависимость react-test-renderer

---
+++ src/components/SmallImage/index.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SmallImage} from './index';
+import renderer from 'react-test-renderer';
+
+test('smallimage test', () => {
+    let component = renderer.create(
+        <SmallImage image = 'string'></SmallImage>,
+    );
+    let title = component.toJSON();
+    expect(title).toMatchSnapshot();
+
+
+});
