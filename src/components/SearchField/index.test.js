1241a1a7a47648eda9e78d51cf592ae2fd1ecac8
mihanizm56 <m727507.56@mail.ru>
2018-12-02 13:05:28 +0400

    fixed

--- src/components/SearchField/index.test.js
+++ src/components/SearchField/index.test.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 
-import  { SearchField }  from '.'
+import { SearchField } from '.'
 
 describe('SearchField', () => {
-	it('Нормальный жизненный цикл', () => {
-		const component = shallow(
-			<SearchField placeholder='lala' 
-						style={{background: '#444'}} 
-						callback={ () => 'callback'}/>)
-		expect(component).toMatchSnapshot()
-		component.unmount()
-	})
+  it('Нормальный жизненный цикл', () => {
+    const component = shallow(
+      <SearchField
+        placeholder="lala"
+        style={{ background: '#444' }}
+        callback={() => 'callback'}
+      />
+    )
+    expect(component).toMatchSnapshot()
+    component.unmount()
+  })
 })
